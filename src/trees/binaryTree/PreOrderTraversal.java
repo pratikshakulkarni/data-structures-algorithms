228b0421bde1c9cfae2298e5c77818c4cfef14e6
Pratiksha Kulkarni <Kulkarnipratiksha1997@gmail.com>
2023-01-07 23:46:31 +0530

    Added BT package and pre-order traversal - iterative approach

--- src/trees/binaryTree/PreOrderTraversal.java
+++ src/trees/binaryTree/PreOrderTraversal.java
@@ -1,48 +1,66 @@
 package trees.binaryTree;
+
 import java.util.*;
 
 class PreNode {
- int data;
- PreNode left, right;
- PreNode(int data) {
-  this.data = data;
-  left = null;
-  right = null;
- }
- PreNode() {
+    int data;
+    PreNode left, right;
 
- }
+    PreNode(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+
+    PreNode() {
+
+    }
 }
 
 public class PreOrderTraversal {
- static void preOrderTrav(PreNode curr, ArrayList < Integer > preOrder) {
-  if (curr == null)
-   return;
+    static void preOrderTrav(PreNode curr, ArrayList<Integer> preOrder) {
+        if (curr == null)
+            return;
 
-  preOrder.add(curr.data);
-  preOrderTrav(curr.left, preOrder);
-  preOrderTrav(curr.right, preOrder);
- }
+        preOrder.add(curr.data);
+        preOrderTrav(curr.left, preOrder);
+        preOrderTrav(curr.right, preOrder);
+    }
 
- public static void main(String args[]) {
+    static void preOderIterative(PreNode root, ArrayList<Integer> preOrder) {
+        if (root == null) return;
+        else {
+            Stack<PreNode> st = new Stack<>();
+            st.push(root);
+            while (!st.isEmpty()) {
+                root = st.pop();
+                preOrder.add(root.data);
+                if (root.right != null) st.push(root.right);
+                if (root.left != null) st.push(root.left);
+            }
+        }
+    }
 
-  PreNode root = new PreNode(1);
-  root.left = new PreNode(2);
-  root.right = new PreNode(3);
-  root.left.left = new PreNode(4);
-  root.left.right = new PreNode(5);
-  root.left.right.left = new PreNode(8);
-  root.right.left = new PreNode(6);
-  root.right.right = new PreNode(7);
-  root.right.right.left = new PreNode(9);
-  root.right.right.right = new PreNode(10);
+    public static void main(String args[]) {
 
-  ArrayList < Integer > preOrder = new ArrayList < > ();
-  preOrderTrav(root, preOrder);
+        PreNode root = new PreNode(1);
+        root.left = new PreNode(2);
+        root.right = new PreNode(3);
+        root.left.left = new PreNode(4);
+        root.left.right = new PreNode(5);
+        root.left.right.left = new PreNode(8);
+        root.right.left = new PreNode(6);
+        root.right.right = new PreNode(7);
+        root.right.right.left = new PreNode(9);
+        root.right.right.right = new PreNode(10);
 
-  System.out.print("The preOrder Traversal is : ");
-  for (int i = 0; i < preOrder.size(); i++) {
-   System.out.print(preOrder.get(i) + " ");
-  }
- }
+        ArrayList<Integer> preOrder = new ArrayList<>();
+        //preOrderTrav(root, preOrder);
+        preOderIterative(root, preOrder);
+
+        System.out.print("The preOrder Traversal is : ");
+        for (int i = 0; i < preOrder.size(); i++) {
+            System.out.print(preOrder.get(i) + " ");
+        }
+    }
 }
